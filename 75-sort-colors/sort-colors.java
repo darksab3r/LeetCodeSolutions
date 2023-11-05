284f1cc76da8bbbdfc0e3e33e0732edc689611a8
darksab3r <80017440+darksab3r@users.noreply.github.com>
2023-11-05 22:33:34 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (81.72%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -7,10 +7,10 @@ class Solution {
         int ind2=n-1;
         int k=0;
         while(k<=ind2){
-            if(nums[k]==0){
+            if(nums[k]==0 &&k>ind0){
                 swap(k,ind0);
                 ind0++;
-                k++;
+                // k++; //to make sure that k isnt going to swap any elemets from 0 to ind0
             }
             else if(nums[k]==2){
                 swap(k,ind2);
