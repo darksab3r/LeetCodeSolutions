72e5ad7876b92841b1fc84dcbbfc8434dbb55b8d
Atul Bartwal <80017440+darksab3r@users.noreply.github.com>
2024-01-06 11:09:02 +0530

    Time: 29 ms (79.41%) | Memory: 55.2 MB (62.93%) - LeetSync

---
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+
+class Solution {
+    public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
+        int n = startTime.length;
+        int[][] jobs = new int[n][3];
+
+        for (int i = 0; i < n; i++) {
+            jobs[i] = new int[]{startTime[i], endTime[i], profit[i]};
+        }
+
+        // Sort jobs based on end times
+        Arrays.sort(jobs, (a, b) -> a[1] - b[1]);
+
+        int[] dp = new int[n];
+        dp[0] = jobs[0][2];
+
+        for (int i = 1; i < n; i++) {
+            int currentProfit = jobs[i][2];
+            int prevNonOverlapIndex = binarySearch(jobs, i);
+            if (prevNonOverlapIndex != -1) {
+                currentProfit += dp[prevNonOverlapIndex];
+            }
+            dp[i] = Math.max(currentProfit, dp[i - 1]);
+        }
+
+        return dp[n - 1];
+    }
+
+    private int binarySearch(int[][] jobs, int index) {
+        int low = 0, high = index - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (jobs[mid][1] <= jobs[index][0]) {
+                if (jobs[mid + 1][1] <= jobs[index][0]) {
+                    low = mid + 1;
+                } else {
+                    return mid;
+                }
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return -1; // No non-overlapping job found
+    }
+}
